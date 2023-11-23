da3b9032085dca4e073b5597ee99191f0a79388e
Diaz027 <dami27cito@gmail.com>
2023-11-23 17:17:10 -0300

    new

--- src/app/api/license/route.js
+++ src/app/api/license/route.js
@@ -1,3 +1,6 @@
+import { NextResponse } from 'next/server';
+import  {prisma } from '@/libs/prisma';
+
 export async function GET() {
   const findLicense = await prisma.license.findMany({
     include: {
