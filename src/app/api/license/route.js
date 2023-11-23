e00a6a9167ceef75bca021b4811d4e9158de44e7
Diaz027 <dami27cito@gmail.com>
2023-11-22 23:30:18 -0300

    new license

--- src/app/api/license/route.js
+++ src/app/api/license/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import  {prisma } from '@/libs/prisma';
 
 export async function GET() {
-    try {
+ 
         const findLicense = await prisma.license.findMany();
           const licensesWithGames = findLicense.map((license) => {
             const gameName = license.Games ? license.Games.title : null;
@@ -12,22 +12,20 @@ export async function GET() {
             };
           });
 
-          return NextResponse.status(200).json(licensesWithGames);
-    } catch (error) {
-        return NextResponse.status(400).send(error.message)
-    }
+          return NextResponse.json(licensesWithGames);
+    
 }
 
-// export async function POST (request) {
-//   const {name, active, date } = await request.json();
-//   console.log("Solicitud recibida:", req.body);
-//       const createLicense = await prisma.license.create({
-//         data: {
-//           name,
-//           date,
-//           active
-//         }
-//         });
+export async function POST (request) {
+  const {name, active, date } = await request.json();
+  console.log("Solicitud recibida:", req.body);
+      const createLicense = await prisma.license.create({
+        data: {
+          name,
+          date,
+          active
+        }
+        });
         
-//         return NextResponse.json(createLicense);
-// }
+        return NextResponse.json(createLicense);
+}
