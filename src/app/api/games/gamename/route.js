f9cbe3dcf3a711f98d23dbfaef23d60e8b57dedf
Diaz027 <dami27cito@gmail.com>
2023-11-24 15:24:42 -0300

    new3

--- src/app/api/games/gamename/route.js
+++ src/app/api/games/gamename/route.js
@@ -1,21 +1,21 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/libs/prisma";
+// import { NextResponse } from "next/server";
+// import { prisma } from "@/libs/prisma";
 
-export async function GET(request) {
-  const url = new URL(request.url);
-  const name = url.searchParams.get("title").toLowerCase();
+// export async function GET(request) {
+//   const url = new URL(request.url);
+//   const name = url.searchParams.get("title").toLowerCase();
 
-  // Buscar en la base de datos cualquier coincidencia
-  const result = await prisma.games.findMany({
-    where: {
-      title: {
-        contains: name,
-      },
-    },
-  });
+//   // Buscar en la base de datos cualquier coincidencia
+//   const result = await prisma.games.findMany({
+//     where: {
+//       title: {
+//         contains: name,
+//       },
+//     },
+//   });
 
-  return NextResponse.json(result);
-}
+//   return NextResponse.json(result);
+// }
 
 // export async function GET(request) {
 //   const url = new URL(request.url);
@@ -37,3 +37,27 @@ export async function GET(request) {
 
 //   return NextResponse.json(filteredResult);
 // }
+//// prueba dami
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+import {data} from '../../data';
+
+export async function GET(request) {
+  const url = new URL(request.url);
+  const name = url.searchParams.get("title").toLowerCase();
+
+  // Buscar en la base de datos cualquier coincidencia
+  const resultDB = await prisma.games.findMany({
+    where: {
+      title: {
+        contains: name,
+      },
+    },
+  });
+
+  const resultData = data.filter(game => game.title.toLowerCase().includes(name));
+
+  const result = [...resultDB, ...resultData];
+
+  return NextResponse.json(result);
+}
