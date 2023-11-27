5e65eb2350762bd09392ed2e98728de1eb327886
Gabriel J. <106175930+zuoki@users.noreply.github.com>
2023-11-27 07:51:39 -0300

    the chamba is te best

--- src/components/details/components_details/Trailer/Trailer.jsx
+++ src/components/details/components_details/Trailer/Trailer.jsx
@@ -5,7 +5,7 @@ import './Trailer.css'
 const Trailer = ({ game }) => {
   return (
     <div className="trailer-container">
-      <iframe width="100%" height="100%" src={game.video} allowfullscreen  style={{ border: '0px' }}></iframe>
+<iframe width="100%" height="100%" src={`${game.video}?controls=0`} allow="autoplay" allowFullScreen style={{ border: '0px' }} muted></iframe>
     </div>
   );
 };
