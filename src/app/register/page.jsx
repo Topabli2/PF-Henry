1df7370336c073098eda53db2d78906de3748c6f
Chrisnatalx <natale.christian.a@gmail.com>
2023-11-27 14:47:25 -0300

    Changes in form

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,5 +1,5 @@
-// Register.js
-"use client"
+/// Register.js
+'use client';
 import React from 'react';
 import './register.css';
 import validation from './validation';
@@ -7,104 +7,135 @@ import Link from 'next/link';
 import { createUserStore } from '@/store/createUserStore';
 
 const Register = () => {
-  const { user, setUser, msjToRender, err, setMsjToRender, setErr } = createUserStore();
+	const { user, setUser, msjToRender, err, setMsjToRender, setErr } =
+		createUserStore();
 
-  const handleUser = (e) => {
-    setUser({
-      [e.target.name]: e.target.value,
-    });
-  };
+	console.log(user);
+	const handleUser = (e) => {
+		setUser({
+			[e.target.name]: e.target.value,
+		});
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    // Validación
-    const validationError = validation(user);
-    if (validationError) {
-      setErr(validationError);
-      return;
-    }
+		// // Validación
+		// const validationError = validation(user);
+		// if (validationError) {
+		// 	setErr(validationError);
+		// 	return;
+		// }
 
-    try {
-      // Envío de datos al backend
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
+		try {
+			// Envío de datos al backend
+			const response = await fetch('/api/users', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(user),
+			});
 
-      if (response.ok) {
-        setMsjToRender('Usuario creado con éxito');
-        setErr('');
-        // Puedes hacer otras cosas aquí, como redirigir al usuario a otra página.
-      } else {
-        const errorData = await response.json();
-        setErr(errorData.message || 'Error al crear el usuario');
-      }
-    } catch (error) {
-      setErr('Error al comunicarse con el servidor');
-    }
-  };
+			if (response.ok) {
+				setMsjToRender('Usuario creado con éxito');
+				setErr('');
+				// Puedes hacer otras cosas aquí, como redirigir al usuario a otra página.
+			} else {
+				const errorData = await response.json();
+				setErr(errorData.message || 'Error al crear el usuario');
+			}
+		} catch (error) {
+			setErr('Error al comunicarse con el servidor');
+		}
+	};
 
-  const handleDisabled = () => {
-    return (
-      err.length > 0 ||
-      Object.values(user).some((value) => value.length === 0)
-    );
-  };
+	// const handleDisabled = () => {
+	// 	return (
+	// 		err.length > 0 || Object.values(user).some((value) => value.length === 0)
+	// 	);
+	// };
 
-  return (
-    <div className="formRegister">
-      <form onSubmit={handleSubmit}>
-	  <p>Registrarse</p>
-                <div className="campos">
-                    <input
-                        autoComplete="off"
-                        onChange={handleUser}
-                        value={user.userName}
-                        name="userName"
-                        placeholder="Username"
-                    />
-                    <br />
-                    <input
-                        autoComplete="off"
-                        onChange={handleUser}
-                        value={user.email}
-                        name="email"
-                        placeholder="email"
-                    />
-                    <br />
-                    <input
-                        type="password"
-                        autoComplete="off"
-                        onChange={handleUser}
-                        value={user.password}
-                        name="password"
-                        placeholder="password"
-                    />
-                    <br />
-                    <input
-                        type="password"
-                        autoComplete="off"
-                        onChange={handleUser}
-                        value={user.repeatPassword}
-                        name="repeatPassword"
-                        placeholder="Repeat password"
-                    />
-                    <br />
-                </div>
-                <Link href={'/'}>
-                    <button disabled={handleDisabled()}>Submit</button>
-                </Link>
-                <p className="msjToRender">{err.length > 0 && err}</p>
-                {msjToRender.length > 0 && <p className="msjToRender">{msjToRender}</p>}
-           
-
-      </form>
-    </div>
-  );
+	return (
+		<div className="formRegister">
+			<form onSubmit={handleSubmit}>
+				<p>Registrarse</p>
+				<div className="campos">
+					{/* Añadir campos adicionales según las props */}
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.username}
+						name="username"
+						placeholder="Username"
+					/>
+					<br />
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.lastName}
+						name="lastName"
+						placeholder="Last Name"
+					/>
+					<br />
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.password}
+						name="password"
+						type="password"
+						placeholder="Password"
+					/>
+					<br />
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.email}
+						name="email"
+						placeholder="Email"
+					/>
+					<br />
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.country}
+						name="country"
+						placeholder="Country"
+					/>
+					<br />
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.phone}
+						name="phone"
+						placeholder="Phone"
+					/>
+					<br />
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.profileName}
+						name="profileName"
+						placeholder="ProfileName"
+					/>
+					<br />
+					<input
+						autoComplete="off"
+						onChange={handleUser}
+						value={user.profile_type}
+						name="profile_type"
+						placeholder="profile_type"
+					/>
+					<br />
+				</div>
+				<Link href={'/'}>
+					<button>Submit</button>
+				</Link>
+				<p className="msjToRender">{err.length > 0 && err}</p>
+				{msjToRender.length > 0 && <p className="msjToRender">{msjToRender}</p>}
+			</form>
+		</div>
+	);
 };
 
 export default Register;
--- src/store/createUserStore.jsx
+++ src/store/createUserStore.jsx
@@ -2,22 +2,29 @@
 import { create } from 'zustand';
 
 export const createUserStore = create((set) => ({
-  user: {
-    userName: '',
-    email: '',
-    password: '',
-    repeatPassword: '',
-  },
-  msjToRender: '',
-  err: '',
-  setUser: (user) =>
-    set((state) => ({
-      ...state,
-      user: {
-        ...state.user,
-        ...user,
-      },
-    })),
-  setMsjToRender: (msj) => set({ msjToRender: msj }),
-  setErr: (error) => set({ err: error }),
+	user: {
+		username: '',
+		lastName: '',
+		password: '',
+		email: '',
+		country: '',
+		phone: '',
+		userGames: [],
+		profile: {
+			profileName: '',
+			profile_type: '',
+		},
+	},
+	msjToRender: '',
+	err: '',
+	setUser: (user) =>
+		set((state) => ({
+			...state,
+			user: {
+				...state.user,
+				...user,
+			},
+		})),
+	setMsjToRender: (msj) => set({ msjToRender: msj }),
+	setErr: (error) => set({ err: error }),
 }));
