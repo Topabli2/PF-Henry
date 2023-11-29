49a011a09eb1074d0d2baec030f0bf9172461540
afradenburg <andres.burg@hotmail.com>
2023-11-29 12:51:04 -0500

    n

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -4,17 +4,12 @@ import "./payment.css";
 import { useState } from "react";
 
 const page = () => {
-
-   const [statebuy,setStateBuy]= useState(" COMPRA EN PROGRESO ");
-   const [css,setCss]= useState("progress");
+  const [statebuy, setStateBuy] = useState(" COMPRA EN PROGRESO ");
+  const [css, setCss] = useState("progress");
 
   return (
     <div className="paypal">
       <div className="visualP">
-
-
-
-
         <h1 className={css}>{statebuy}</h1>
         <PayPalScriptProvider
           options={{
@@ -35,16 +30,15 @@ const page = () => {
             }}
             onApprove={(data, actions) => {
               actions.order.capture();
-              setStateBuy("COMPRA REALIZADA CON EXITO")
-              setCss("finish")
-              
-              //poner aqui el envio de gmail 
+              setStateBuy("COMPRA REALIZADA CON EXITO");
+              setCss("finish");
 
+              //poner aqui el envio de gmail
             }}
             onCancel={(data) => {
               console.log(data);
-              setStateBuy("COMPRA CANCELADA")
-              setCss("cancel")
+              setStateBuy("COMPRA CANCELADA");
+              setCss("cancel");
             }}
           />
         </PayPalScriptProvider>
