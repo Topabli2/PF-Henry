a13983d868b5e7cd14a7fccbf647db583a0343fe
Chrisnatalx <natale.christian.a@gmail.com>
2023-11-29 12:46:17 -0300

    Fix bug fetching data in Detail Component

--- src/app/details/[id]/page.jsx
+++ src/app/details/[id]/page.jsx
@@ -4,13 +4,12 @@ import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Page = async ({ params }) => {
-    const response = await axios(`http://localhost:3000/api/games/${params.id}`);
-    return (
-        <div>
-            <Details game={response.data[0]} />
-        </div>
-    );
+	const response = await axios(`http://localhost:3000/api/games/${params.id}`);
+	return (
+		<div>
+			<Details game={response.data[0]} />
+		</div>
+	);
 };
 
 export default Page;
-
