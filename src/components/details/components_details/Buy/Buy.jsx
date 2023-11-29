9b5c92aee515df73ef05229ae0bd09d6a81809ef
Diego <martinsdiego0513@outlook.es>
2023-11-29 16:52:13 -0300

    Integración zustand

--- src/components/details/components_details/Buy/Buy.jsx
+++ src/components/details/components_details/Buy/Buy.jsx
@@ -1,23 +1,32 @@
+'use client';
 import React from 'react'
 import "./Buy.css"
-import ButtonComponent from '@/components/utils/button_Component/ButtonComponent'
+import ButtonComponent from '@/components/utils/button_Component/ButtonComponent';
+import { useStoreCart, useStoreGame } from '@/zustand/store';
+import Link from 'next/link';
 
-const Buy = ({game}) => {
+const Buy = ({ game }) => {
+  const { gamesInCart, addGamesToCart } = useStoreCart();
+
+  const handleCart = (game) => {
+    addGamesToCart(game);
+  }
+
   return (
     <section className='Dbuy'>
-        
-         <div className='Dbuy2'>
+
+      <div className='Dbuy2'>
         <h2 className='DBof'> CURRENT OFFER</h2>
         <div className='priceContainerD'>
           <h1 className='DBPRC'>BUY FOR : ${game.price}</h1>
         </div>
         <div className='butonDBJContainer'>
-          <button type='button'className='ADC'>  ADD TO CART</button>
-          <button type='button' className='IAC'>GO TO CART</button> 
+          <button type='button' className='ADC' onClick={() => handleCart(game)} >  ADD TO CART</button>
+          <Link href={'/cart'}><button type='button' className='IAC'>GO TO CART</button></Link>
         </div>
-        </div>
-            
-        </section>
+      </div>
+
+    </section>
   )
 }
 
--- src/zustand/store/index.js
+++ src/zustand/store/index.js
@@ -1,5 +1,24 @@
 import create from 'zustand';
 
-const [useStore] = create(set => ({
-    
-}))
+export const useStoreCart = create((set) => ({
+    gamesInCart: [],
+    addGamesToCart: (games) => {
+        set((state) => ({
+            gamesInCart: [...state.gamesInCart, games],
+        }));
+    },
+    removeGameFromCart: (gameID) => {
+        set((state) => ({
+            gamesInCart: state.gamesInCart.filter((game) => game.id !== gameID),
+        }));
+    },
+}));
+
+export const useStoreGame = create((set) => ({
+    gameToBack: [],
+    addGameToBack: (game) => {
+        set((state) => ({
+            gameToBack: [...state.gameToBack, game],
+        }));
+    },
+}));
