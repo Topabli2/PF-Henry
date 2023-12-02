701461c5dcf561fce2c3302f72b5461eca370bba
Rdebora <riosdeborasabrina@gmail.com>
2023-12-02 10:36:28 -0300

    storageFinally

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -1,77 +1,82 @@
-'use client';
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-import { useUser } from '@clerk/nextjs';
-import axios from 'axios';
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import { useStoreCart } from "@/zustand/store";
 
 const Payment = () => {
-
-    const [statebuy, setStateBuy] = useState(" COMPRA EN PROGRESO ");
-    const [css, setCss] = useState("progress");
-    const data = useUser();
+  const [statebuy, setStateBuy] = useState(" COMPRA EN PROGRESO ");
+  const [css, setCss] = useState("progress");
+  const data = useUser();
   const email = data?.user?.emailAddresses?.[0]?.emailAddress;
-// const email = "pabloverat2@gmail.com"
- 
+  // const email = "pabloverat2@gmail.com"
+  const { emptyCart } = useStoreCart();
 
-    return (
-        <div className='paypal'>
-            <div className="visualP">
-                <h1 className={css}>{statebuy}</h1>
-                <PayPalScriptProvider
-                    options={{
-                        clientId:
-                            "AVwb2hp2ZMkuRiQgJ1GujcCeizroHgzH-pOwUlWYdCNmPNcgRPaDjwrVg4BfI_k98Qd4DtUVpsYCquD8",
-                    }}
-                >
-                    <PayPalButtons
-                        style={{
-                            label: "pay",
-                        }}
-                        createOrder={async () => {
-                            const res = await fetch("/api/checkout", {
-                                method: "POST",
-                            });
-                            const order = await res.json();
-                            return order.id;
-                        }}
-                        onApprove={async (data, actions) => {
-                            // agregamos async
-                            await actions.order.capture(); // agregamos await *Debbb
-                            setStateBuy("COMPRA REALIZADA CON EXITO");
-                            setCss("finish");
+  return (
+    <div className="paypal">
+      <div className="visualP">
+        <h1 className={css}>{statebuy}</h1>
+        <PayPalScriptProvider
+          options={{
+            clientId:
+              "AVwb2hp2ZMkuRiQgJ1GujcCeizroHgzH-pOwUlWYdCNmPNcgRPaDjwrVg4BfI_k98Qd4DtUVpsYCquD8",
+          }}
+        >
+          <PayPalButtons
+            style={{
+              label: "pay",
+            }}
+            createOrder={async () => {
+              const res = await fetch("/api/checkout", {
+                method: "POST",
+              });
+              const order = await res.json();
+              return order.id;
+            }}
+            onApprove={async (data, actions) => {
+              // agregamos async
+              await actions.order.capture(); // agregamos await *Debbb
+              setStateBuy("COMPRA REALIZADA CON EXITO");
+              setCss("finish");
+              // Vacía el carrito de compras después de que se haya realizado un pago exitoso
+              emptyCart();
 
-                            //poner aqui el envio de gmail
-                            // Hacer una solicitud al back-end para enviar un correo electrónico
-                            
-                            await axios.post("/api/sendEmail", { email }, {
-                                headers: {
-                                  "Content-Type": "application/json",
-                                },
-                              });
-                     
-                        }}
-                        // fin bloque backend
-                        onCancel={async (data) => {
-                            console.log(data);
-                            setStateBuy("COMPRA CANCELADA");
-                            setCss("cancel");
+              //poner aqui el envio de gmail
+              // Hacer una solicitud al back-end para enviar un correo electrónico
 
-                            // Hacer una solicitud al back-end para enviar un correo electrónico
-                            await fetch("/api/sendEmail", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify({
-                                    email
-                                }),
-                            });
-                        }}
-                    />
-                </PayPalScriptProvider>
-            </div>
-        </div>
-    )
-}
+              await axios.post(
+                "/api/sendEmail",
+                { email },
+                {
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              );
+            }}
+            // fin bloque backend
+            onCancel={async (data) => {
+              console.log(data);
+              setStateBuy("COMPRA CANCELADA");
+              setCss("cancel");
 
-export default Payment
+              // Hacer una solicitud al back-end para enviar un correo electrónico
+              await fetch("/api/sendEmail", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  email,
+                }),
+              });
+            }}
+          />
+        </PayPalScriptProvider>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
--- src/zustand/store/index.js
+++ src/zustand/store/index.js
@@ -1,4 +1,4 @@
-import { create } from 'zustand';
+/*import { create } from 'zustand';
 
 export const useStoreCart = create((set) => ({
     gamesInCart: [],
@@ -26,4 +26,60 @@ export const useStoreGame = create((set) => ({
             gameToBack: [...state.gameToBack, game],
         }));
     },
+}));*/
+
+
+// Importamos la librería zustand para manejar el estado global de la aplicación.
+import create from 'zustand';
+
+// Creamos un store con zustand. Este store manejará el estado del carrito de compras.
+export const useStoreCart = create((set) => ({
+    // Inicializamos el estado del carrito de compras con los juegos que están en el localStorage.
+    // Si no hay juegos en el localStorage, inicializamos con un array vacío.
+    gamesInCart: typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('gamesInCart')) || [] : [],
+
+    // Esta función agrega un juego al carrito de compras.
+    addGamesToCart: (games) => {
+        set((state) => {
+            // Creamos un nuevo array con los juegos que ya estaban en el carrito y el nuevo juego.
+            const newGamesInCart = [...state.gamesInCart, games];
+
+            // Guardamos el nuevo estado del carrito en el localStorage.
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('gamesInCart', JSON.stringify(newGamesInCart));
+            }
+
+            // Actualizamos el estado del carrito en el store.
+            return { gamesInCart: newGamesInCart };
+        });
+    },
+
+    // Esta función elimina un juego del carrito de compras.
+    removeGameFromCart: (gameID) => {
+        set((state) => {
+            // Creamos un nuevo array con todos los juegos que no tienen el id del juego a eliminar.
+            const newGamesInCart = state.gamesInCart.filter((game) => game.id !== gameID);
+
+            // Guardamos el nuevo estado del carrito en el localStorage.
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('gamesInCart', JSON.stringify(newGamesInCart));
+            }
+
+            // Actualizamos el estado del carrito en el store.
+            return { gamesInCart: newGamesInCart };
+        });
+    },
+
+    // Esta función vacía el carrito de compras.
+    emptyCart: () => {
+        set(() => {
+            // Eliminamos el estado del carrito del localStorage.
+            if (typeof window !== 'undefined') {
+                localStorage.removeItem('gamesInCart');
+            }
+
+            // Actualizamos el estado del carrito en el store a un array vacío.
+            return { gamesInCart: [] };
+        });
+    }
 }));
