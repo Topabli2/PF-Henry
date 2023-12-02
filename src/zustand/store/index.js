da596f99e794372cbd4ef17f026562fec61043af
Rdebora <riosdeborasabrina@gmail.com>
2023-12-02 12:02:44 -0300

    storateterminadodeveritas

--- src/zustand/store/index.js
+++ src/zustand/store/index.js
@@ -29,57 +29,49 @@ export const useStoreGame = create((set) => ({
 }));*/
 
 
+
 // Importamos la librería zustand para manejar el estado global de la aplicación.
-import create from 'zustand';
+import {create} from 'zustand';
 
-// Creamos un store con zustand. Este store manejará el estado del carrito de compras.
+// Asumiendo que tienes una función que obtiene el userId del usuario actual.
+//import { useUser } from '@clerk/nextjs'; // Asegúrate de reemplazar esto con tu propio módulo de autenticación.
+
+
 export const useStoreCart = create((set) => ({
-    // Inicializamos el estado del carrito de compras con los juegos que están en el localStorage.
-    // Si no hay juegos en el localStorage, inicializamos con un array vacío.
-    gamesInCart: typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('gamesInCart')) || [] : [],
+    userId: null,
+    gamesInCart: [],
 
-    // Esta función agrega un juego al carrito de compras.
+    setUserId: (id) => set((state) => {
+        const storedGamesInCart = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('gamesInCart' + id)) : [];
+        return { userId: id, gamesInCart: storedGamesInCart || [] };
+    }),
+
     addGamesToCart: (games) => {
         set((state) => {
-            // Creamos un nuevo array con los juegos que ya estaban en el carrito y el nuevo juego.
             const newGamesInCart = [...state.gamesInCart, games];
-
-            // Guardamos el nuevo estado del carrito en el localStorage.
             if (typeof window !== 'undefined') {
-                localStorage.setItem('gamesInCart', JSON.stringify(newGamesInCart));
+                localStorage.setItem('gamesInCart' + state.userId, JSON.stringify(newGamesInCart));
             }
-
-            // Actualizamos el estado del carrito en el store.
             return { gamesInCart: newGamesInCart };
         });
     },
 
-    // Esta función elimina un juego del carrito de compras.
     removeGameFromCart: (gameID) => {
         set((state) => {
-            // Creamos un nuevo array con todos los juegos que no tienen el id del juego a eliminar.
             const newGamesInCart = state.gamesInCart.filter((game) => game.id !== gameID);
-
-            // Guardamos el nuevo estado del carrito en el localStorage.
             if (typeof window !== 'undefined') {
-                localStorage.setItem('gamesInCart', JSON.stringify(newGamesInCart));
+                localStorage.setItem('gamesInCart' + state.userId, JSON.stringify(newGamesInCart));
             }
-
-            // Actualizamos el estado del carrito en el store.
             return { gamesInCart: newGamesInCart };
         });
     },
 
-    // Esta función vacía el carrito de compras.
     emptyCart: () => {
-        set(() => {
-            // Eliminamos el estado del carrito del localStorage.
+        set((state) => {
             if (typeof window !== 'undefined') {
-                localStorage.removeItem('gamesInCart');
+                localStorage.removeItem('gamesInCart' + state.userId);
             }
-
-            // Actualizamos el estado del carrito en el store a un array vacío.
             return { gamesInCart: [] };
         });
-    }
+    },
 }));
