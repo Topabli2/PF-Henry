9fccb89e481c3968a45a212345813925e12a2d8a
topabli2 <pabloverat2@gmail.com>
2023-12-03 19:53:37 -0300

    zustand/store/index.js change

--- src/components/details/components_details/Buy/Buy.jsx
+++ src/components/details/components_details/Buy/Buy.jsx
@@ -1,9 +1,9 @@
-'use client';
-import React from 'react';
-import './Buy.css';
-import ButtonComponent from '@/components/utils/button_Component/ButtonComponent';
+"use client";
+import React from "react";
+import "./Buy.css";
+import ButtonComponent from "@/components/utils/button_Component/ButtonComponent";
 // import { useStoreCart, useStoreGame } from '@/zustand/store';
-import Link from 'next/link';
+import Link from "next/link";
 
 // const Buy = ({ game }) => {
 //   const { gamesInCart, addGamesToCart } = useStoreCart();
@@ -33,49 +33,50 @@ import Link from 'next/link';
 // export default Buy
 
 // import React from 'react';
-import { useStoreCart } from '@/zustand/store';
+import { useStoreCart } from "@/zustand/store";
 // import Link from 'next/link';
-import { useUser } from '@clerk/nextjs';
+import { useUser } from "@clerk/nextjs";
 
 const Buy = ({ game }) => {
-	const { addGamesToCart } = useStoreCart();
-	const user = useUser();
+  console.log(game);
+  const { addGamesToCart } = useStoreCart();
+  const user = useUser();
 
-	const handleCart = () => {
-		if (user?.isSignedIn) {
-			addGamesToCart(game);
-		} else {
-			window.location.href = '/sign-in'; // Redirige al usuario a la página de registro
-		}
-	};
+  const handleCart = () => {
+    if (user?.isSignedIn) {
+      addGamesToCart(game);
+    } else {
+      window.location.href = "/sign-in"; // Redirige al usuario a la página de registro
+    }
+  };
 
-	return (
-		<section className="Dbuy">
-			<div className="Dbuy2">
-				<h2 className="DBof"> CURRENT OFFER</h2>
-				<div className="priceContainerD">
-					<h1 className="DBPRC">BUY FOR: ${game.price}</h1>
-				</div>
-				<div className="butonDBJContainer">
-					<button type="button" className="ADC" onClick={handleCart}>
-						ADD TO CART
-					</button>
+  return (
+    <section className="Dbuy">
+      <div className="Dbuy2">
+        <h2 className="DBof"> CURRENT OFFER</h2>
+        <div className="priceContainerD">
+          <h1 className="DBPRC">BUY FOR: ${game.price}</h1>
+        </div>
+        <div className="butonDBJContainer">
+          <button type="button" className="ADC" onClick={handleCart}>
+            ADD TO CART
+          </button>
 
-					{user?.isSignedIn ? (
-						<Link href="/cart">
-							<button type="button" className="IAC">
-								GO TO CART
-							</button>
-						</Link>
-					) : (
-						<button type="button" className="IACDISABLED" disabled>
-							GO TO CART
-						</button>
-					)}
-				</div>
-			</div>
-		</section>
-	);
+          {user?.isSignedIn ? (
+            <Link href="/cart">
+              <button type="button" className="IAC">
+                GO TO CART
+              </button>
+            </Link>
+          ) : (
+            <button type="button" className="IACDISABLED" disabled>
+              GO TO CART
+            </button>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Buy;
--- src/zustand/store/index.js
+++ src/zustand/store/index.js
@@ -28,14 +28,11 @@ export const useStoreGame = create((set) => ({
     },
 }));*/
 
-
-
 // Importamos la librería zustand para manejar el estado global de la aplicación.
-import {create} from 'zustand';
+import { create } from "zustand";
 
 // Asumiendo que tienes una función que obtiene el userId del usuario actual.
 //import { useUser } from '@clerk/nextjs'; // Asegúrate de reemplazar esto con tu propio módulo de autenticación.
-
 
 /*export const useStoreCart = create((set) => ({
     userId: null,
@@ -76,56 +73,66 @@ import {create} from 'zustand';
     },
 }));*/
 
-
 export const useStoreCart = create((set) => ({
-    userId: null,
-    gamesInCart: [],
- //Storage
-    setUserId: (id) => set((state) => {
-        const storedGamesInCart = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('gamesInCart' + id)) : [];
-        return { userId: id, gamesInCart: storedGamesInCart || [] };
+  userId: null,
+  gamesInCart: [],
+  //Storage
+  setUserId: (id) =>
+    set((state) => {
+      const storedGamesInCart =
+        typeof window !== "undefined"
+          ? JSON.parse(localStorage.getItem("gamesInCart" + id))
+          : [];
+      return { userId: id, gamesInCart: storedGamesInCart || [] };
     }),
 
-    //Storage 
-    addGamesToCart: (games) => {
-        set((state) => {
-            let condicion = false;
+  //Storage
+  addGamesToCart: (games) => {
+    set((state) => {
+      let condicion = false;
 
-            if(JSON.parse(localStorage.gamesInCartnull).length === 4) {
-                console.log(JSON.parse(localStorage.gamesInCartnull))
-                condicion = true;
-                return state;
-            }
-            if (state.gamesInCart.length < 4) {
-                const newGamesInCart = [...state.gamesInCart, games];
-                if (typeof window !== 'undefined') {
-                    localStorage.setItem('gamesInCart' + state.userId, JSON.stringify(newGamesInCart));
-                }
-                return { gamesInCart: newGamesInCart };
-            } else {
-                alert("No se pueden agregar más de 4 productos al carrito.");
-                return state;
-            }
-        });
-    },
+      if (JSON.parse(localStorage.gamesInCartnull).length === 4) {
+        console.log(JSON.parse(localStorage.gamesInCartnull));
+        condicion = true;
+        return state;
+      }
+      if (state.gamesInCart.length < 4) {
+        const newGamesInCart = [...state.gamesInCart, games];
+        if (typeof window !== "undefined") {
+          localStorage.setItem(
+            "gamesInCart" + state.userId,
+            JSON.stringify(newGamesInCart)
+          );
+        }
+        return { gamesInCart: newGamesInCart };
+      } else {
+        alert("No se pueden agregar más de 4 productos al carrito.");
+        return state;
+      }
+    });
+  },
 
-    removeGameFromCart: (gameID) => {
-        set((state) => {
-            const newGamesInCart = state.gamesInCart.filter((game) => game.id !== gameID);
-            if (typeof window !== 'undefined') {
-                localStorage.setItem('gamesInCart' + state.userId, JSON.stringify(newGamesInCart));
-            }
-            return { gamesInCart: newGamesInCart };
-        });
-    },
+  removeGameFromCart: (gameID) => {
+    set((state) => {
+      const newGamesInCart = state.gamesInCart.filter(
+        (game) => game.id !== gameID
+      );
+      if (typeof window !== "undefined") {
+        localStorage.setItem(
+          "gamesInCart" + state.userId,
+          JSON.stringify(newGamesInCart)
+        );
+      }
+      return { gamesInCart: newGamesInCart };
+    });
+  },
 
-    emptyCart: () => {
-        set((state) => {
-            if (typeof window !== 'undefined') {
-                localStorage.removeItem('gamesInCart' + state.userId);
-            }
-            return { gamesInCart: [] };
-        });
-    },
+  emptyCart: () => {
+    set((state) => {
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("gamesInCart" + state.userId);
+      }
+      return { gamesInCart: [] };
+    });
+  },
 }));
-
