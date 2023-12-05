0cd48104eb2b030ddda1ea4c1060206f99c6d392
Diaz027 <dami27cito@gmail.com>
2023-12-05 20:49:00 -0300

    creadte desde Clerk

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,45 +1,24 @@
 import { NextResponse } from "next/server";
 import  {prisma } from '@/libs/prisma';
 
-// export async function POST(request) {
-//   const data = await request.json();
-//   // console.log("POSTEANDO INFO..");
-//   // console.log(data);
-//   return NextResponse.json(data);
-// }
-
-// export async function POST (request) {
-//     const data = await request.json();
-
-//       // Insertar nuevo usuario en la base de datos
-//       const newUser = await prisma.user.create({
-//         data: {
-//           user_id: data.user.id,
-//           email:  data.user.primaryEmailAddress.emailAddress,
-//           username: data.user.username
-//           // Aquí puedes agregar más campos según los datos que recibas en el webhook
-//         },
-//       });
-//       console.log("Usuario creado!!!!!!!", newUser);
-//       return NextResponse.json(newUser);
+export async function POST(request) {
+  const {data} = await request.json();
+  console.log("POSTEANDO INFO..");
+  console.log(data);
+  if (data ) {
+    // Insertar nuevo usuario en la base de datos
+    const newUser = await prisma.user.create({
+      data: {
+        user_id: data?.id,
+        email:  data?.email_addresses[0]?.email_address,
+        username: data?.username
+      },
+    });
+    console.log("Usuario creado!!!!!!!", newUser);
     
-// }
-
-// export async function POST(request) {
-//   const data = await request.json();
-//   console.log("POSTEANDO INFO..");
-//   console.log(data);
-
-//   // Insertar nuevo usuario en la base de datos
-//   const newUser = await prisma.user.create({
-//     data: {
-//       user_id: data?.user?.id,
-//       email:  data?.user?.email_addresses?.[0]?.emailAddress,
-//       username: data?.user?.username
-//       // Aquí puedes agregar más campos según los datos que recibas en el webhook
-//     },
-//   });
-//   console.log("Usuario creado!!!!!!!", newUser);
+    return NextResponse.json(newUser);
+  } else {
+    console.log("No se pudo crear el usuario. data o data.user no existen.");
+  }
   
-//   return NextResponse.json(newUser);
-// }
+}
