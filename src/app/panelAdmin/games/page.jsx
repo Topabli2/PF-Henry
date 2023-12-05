7d0d8085eda82ee8b17c48dc08efc97ff9913878
Diego <martinsdiego0513@outlook.es>
2023-12-05 19:56:01 -0300

    Panel de admin

--- src/app/panelAdmin/games/page.jsx
+++ src/app/panelAdmin/games/page.jsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
 import { data } from "../../api/data";
-import './games.css'
+import './games.css';
+import { FaRegEye } from "react-icons/fa";
 
 const page = () => {
   return (
     <div className='gamesContainer' >
-
+      
       <div className="gridGames" >
         {data.length > 0 && 
           data.map(game => (
             <div className="gameDiv" >
               <p>{game.title}</p>
               <img src={game.image} />
+              <Link href={'/'}><span className="eyeGame" ><FaRegEye /></span></Link>
             </div>
           ))
         }
